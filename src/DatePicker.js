5174de4b29ef380f7d533484f0d4c65ac6dee056
B0829007 <a0927373726@gmail.com>
2021-05-24 22:43:14 +0800

    First Commit

---
+++ src/DatePicker.js
@@ -0,0 +1,52 @@
+import React,{useState} from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(0),
+    width: 170
+  },
+  textField2: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(0),
+    width: 135
+  }
+}));
+
+function TaiwanDate(date) {
+    const [year, month, day] = date.split('-')
+    return `民國${year - 1911}年-${month}月-${day}號`
+}
+
+export default function DatePickers() {
+  const classes = useStyles();
+  const [myDate, setMydate] = useState("民國110年-05月-24號")
+
+  return (
+    <form className={classes.container} noValidate>
+      <TextField value={myDate}
+        id="basic"
+        label="阿公店開店日選擇"
+        className={classes.textField}
+        />
+      <TextField
+        id="date"
+        label=" "
+        type="date"
+        className={classes.textField2}
+        onChange={event => {
+            setMydate(TaiwanDate(event.target.value))
+        }}
+        InputLabelProps={{
+          shrink: true
+        }}
+      />
+    </form>
+  );
+}
